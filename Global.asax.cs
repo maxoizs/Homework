31647e46cef7ce141c002b1571e5c75a5c4917fc
Mohammad Mahmoud <mohammad@configit.com>
2012-02-27 17:48:17 +0000

    new project ;:q

---
+++ Global.asax.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NewMVC
+{
+     
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+           
+            routes.MapRoute(
+                "Default",  "{controller}/{action}/{id}", new { controller = "Default", action = "Index", id = UrlParameter.Optional } );
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
